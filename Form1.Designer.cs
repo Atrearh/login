2af0367586ffc9d7b588853a54ab311e18115cec
Семен <sem@EXPRESS.local>
2019-05-03 23:13:42 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class FormAuth
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuth));
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.checkBoxLoginRemeber = new System.Windows.Forms.CheckBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(95, 140);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Вход";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(84, 46);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(111, 20);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(84, 90);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.PasswordChar = '*';
+            this.textBoxPass.Size = new System.Drawing.Size(111, 20);
+            this.textBoxPass.TabIndex = 2;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonExit.BackgroundImage")));
+            this.buttonExit.Location = new System.Drawing.Point(269, 0);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(36, 25);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // checkBoxLoginRemeber
+            // 
+            this.checkBoxLoginRemeber.AutoSize = true;
+            this.checkBoxLoginRemeber.Location = new System.Drawing.Point(84, 117);
+            this.checkBoxLoginRemeber.Name = "checkBoxLoginRemeber";
+            this.checkBoxLoginRemeber.Size = new System.Drawing.Size(111, 17);
+            this.checkBoxLoginRemeber.TabIndex = 4;
+            this.checkBoxLoginRemeber.Text = "Запомнить меня";
+            this.checkBoxLoginRemeber.UseVisualStyleBackColor = true;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(33, 49);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(41, 13);
+            this.labelLogin.TabIndex = 5;
+            this.labelLogin.Text = "Логин ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Пароль";
+            // 
+            // FormAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(308, 206);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.checkBoxLoginRemeber);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.buttonLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAuth";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            //((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.CheckBox checkBoxLoginRemeber;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp2
+{
+    public partial class FormAuth : Form
+    {
+        public FormAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+            //solt for md5 hashing =>   _solt
+        }
+
+
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            var isAuth = Authorization(textBoxLogin.Text, textBoxPass.Text);
+            // var message = string.Empty;
+            var message = "Неверный логин или пароль";
+            if (isAuth)
+            {
+                message = "Вход";
+            }
+
+            MessageBox.Show(message);
+        }
+
+        private bool Authorization(string login, string pass)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var dict = GetCredenhtiolString();
+                
+                if (!dict.ContainsKey(login))
+                {
+                    return false;
+                }
+                var originalPass = dict[login];
+                string hash = GetMd5Hash(md5Hash, pass + "solt");
+                if (originalPass != hash)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+        }
+
+        private Dictionary<string, string> GetCredenhtiolString()
+        {
+            var result = new Dictionary<string, string>();
+            string pathDbLogin = "F:/DB.csv";
+            string text = System.IO.File.ReadAllText(pathDbLogin);
+            foreach (var itemLogin in text.Split('\n'))
+            {
+                var authLog = itemLogin.Split(',');
+
+                result.Add(authLog[0].Trim(), authLog[1].Trim());
+            }
+
+            return result;
+
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        
+    }
+
+}
+
+        
+       
+            
+ 
